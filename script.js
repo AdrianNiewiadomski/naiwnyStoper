5b9531aa0f7ac3a1eddc0e7cb7ee067f360e6f36
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2019-10-29 20:20:40 +0100

    Utworzono zmienną timerIsOn i funkcję toggleTimer() zmieniającą napis na buttonie startStop

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("dzialam");
+// alert("dzialam");
+var timerIsOn = false;
+
+function toggleTimer(){
+    timerIsOn = !timerIsOn;
+
+    if(timerIsOn){
+        document.getElementById('startStop').innerHTML = 'Stop';
+    } else {
+        document.getElementById('startStop').innerHTML = 'Start';
+    }
+}
