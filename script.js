e2aa0ba4900691e9b64e25ee251d10a3694f8df3
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2019-10-29 20:44:44 +0100

    Dodano zmienną seconds i funkcję timerRun() zwiększającą ilość sekund o 1 i podmieniającą tekst diva

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // alert("dzialam");
 var timerIsOn = false;
+var seconds = 0;
+
+function timerRun(){
+    seconds++;
+
+    document.getElementById('moj_div').innerHTML =   "00 : " + seconds;
+}
 
 function toggleTimer(){
     timerIsOn = !timerIsOn;
 
     if(timerIsOn){
         document.getElementById('startStop').innerHTML = 'Stop';
+        setTimeout('timerRun()', 1000);
     } else {
         document.getElementById('startStop').innerHTML = 'Start';
     }
